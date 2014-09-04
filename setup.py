4af0eb5190143049fc467d1fb00ed8cb7c63991a
Timothy Morton <tim.morton@gmail.com>
2014-09-04 14:23:54 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "isochrones",
+    version = "0.1",
+    description = "Defines objects for interpolating stellar model grids.",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tim.morton@gmail.com",
+    url = "https://github.com/timothydmorton/isochrones",
+    packages = find_packages(),
+    package_data = {'':['data/*']},
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    install_requires=['plotutils','pandas','astropy','emcee'],
+    zip_safe=False
+) 
