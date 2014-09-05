982443b29ca123b1101c5eb52d077d44231efb25
Timothy Morton <tim.morton@gmail.com>
2014-09-05 17:33:51 +0200

    minor changes

--- isochrones/isochrones.py
+++ isochrones/isochrones.py
@@ -24,22 +24,6 @@ G = const.G.cgs.value
 MSUN = const.M_sun.cgs.value
 RSUN = const.R_sun.cgs.value
 
-DATADIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
-
-#Read data defining extinction in different bands (relative to A_V)
-EXTINCTIONFILE = '{}/extinction.txt'.format(DATADIR)
-EXTINCTION = dict()
-EXTINCTION5 = dict()
-for line in open(EXTINCTIONFILE,'r'):
-    line = line.split()
-    EXTINCTION[line[0]] = float(line[1])
-    EXTINCTION5[line[0]] = float(line[2])
-
-EXTINCTION['kep'] = 0.85946
-EXTINCTION['V'] = 1.0
-EXTINCTION['Ks'] = EXTINCTION['K']
-EXTINCTION['Kepler'] = EXTINCTION['kep']
-
 
 
 class Isochrone(object):
