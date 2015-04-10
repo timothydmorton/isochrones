d7f71e9e2296f352d3f3b7c8fdbcaa3a40623263
Timothy Morton <tim.morton@gmail.com>
2015-04-10 15:09:43 -0400

    added more to __all__

--- isochrones/__init__.py
+++ isochrones/__init__.py
@@ -6,7 +6,9 @@ except NameError:
     __ISOCHRONES_SETUP__ = False
 
 if not __ISOCHRONES_SETUP__:
-    __all__ = ['dartmouth','basti','padova']
+    __all__ = ['dartmouth','basti','padova',
+               'Isochrone', 'StarModel', 'BinaryStarModel',
+               'TripleStarModel']
     from .isochrone import Isochrone
     from .starmodel import StarModel, BinaryStarModel, TripleStarModel
      
