41a7ae9a677bf31bfad8fe228e06eb936b06ab09
Timothy Morton <tim.morton@gmail.com>
2015-04-13 13:23:51 -0400

    trying to make methods pickleable

--- isochrones/starmodel.py
+++ isochrones/starmodel.py
@@ -34,6 +34,28 @@ except ImportError:
 
 from .extinction import EXTINCTION
 
+#some magic to make methods pickleable?
+#from http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+    try:
+        func = cls.__dict__[func_name]
+    except KeyError:
+        pass
+    else:
+        break
+    return func.__get__(obj, cls)
+
+import copy_reg
+import types
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+
 class StarModel(object):
     """An object to represent a star, with observed properties, modeled by an Isochrone
 
