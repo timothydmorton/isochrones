bf30e07b1e7fd7cc43b132a7ca0595e005592261
Timothy Morton <tim.morton@gmail.com>
2016-10-05 14:25:43 -0400

    making ISOCHRONES if does not exist

--- isochrones/config.py
+++ isochrones/config.py
@@ -3,3 +3,6 @@ import os
 ISOCHRONES = os.getenv('ISOCHRONES',
                        os.path.expanduser(os.path.join('~','.isochrones')))
 
+if not os.path.exists(ISOCHRONES):
+    os.makedirs(ISOCHRONES)
+    
