1bfce608de9d12225bd2c0a18c39e158c45f0235
Timothy Morton <tim.morton@gmail.com>
2016-10-10 16:28:50 -0400

    abandoned fileutils; everything now in ModelGrid

--- isochrones/dartmouth/grids.py
+++ isochrones/dartmouth/grids.py
@@ -1,14 +1,12 @@
-import os,re
+import os,re, glob
 import numpy as np
 import pandas as pd
 import logging
 
-from .fileutils import get_hdf
-
-def get_grid():
-    pass
+from ..config import ISOCHRONES
 
 class ModelGrid(object):
+    name = 'dartmouth'
     common_columns = ('EEP', 'MMo', 'LogTeff', 'LogG', 'LogLLo', 'age', 'feh')
     phot_systems = ('SDSSugriz','UBVRIJHKsKp','WISE','LSST','UKIDSS')
     phot_bands = dict(SDSSugriz=['sdss_z', 'sdss_i', 'sdss_r', 'sdss_u', 'sdss_g'],
@@ -18,6 +16,10 @@ class ModelGrid(object):
                   UKIDSS=['Y', 'H', 'K', 'J', 'Z'])
 
     default_kwargs = {'afe':'afep0', 'y':''}
+    datadir = os.path.join(ISOCHRONES, 'dartmouth')
+    zenodo_record = 159426
+    zenodo_files = ('dartmouth.tgz', 'dartmouth.tri')
+    master_tarball_file = 'dartmouth.tgz'
 
     def __init__(self, bands, **kwargs):
         self.bands = bands
@@ -81,7 +83,7 @@ class ModelGrid(object):
             s,b = self.get_band(bnd)
             logging.debug('loading {} band from {}'.format(b,s))
             if s not in grids:
-                grids[s] = get_hdf(s, **self.kwargs)
+                grids[s] = self.get_hdf(s, **self.kwargs)
             if 'MMo' not in df:
                 df[list(self.common_columns)] = grids[s][list(self.common_columns)]
             col = grids[s][b]
@@ -91,5 +93,112 @@ class ModelGrid(object):
             df.loc[:, bnd] = col.values #dunno why it has to be this way; something
                                         # funny with indexing.
 
+        return df
+
+
+    def download_grids(self, overwrite=True):
+        record = self.zenodo_record
+
+        paths = []
+        urls = []
+        for f in self.zenodo_files:
+            paths.append(os.path.join(ISOCHRONES, f))
+            urls.append('https://zenodo.org/record/{}/files/{}'.format(record, f))
+
+        from six.moves import urllib
+        print('Downloading {} stellar model data (should happen only once)...'.format(self.name))
+
+        for path, url in zip(paths, urls):
+            if os.path.exists(path):
+                if overwrite:
+                    os.remove(path)
+                else:
+                    continue
+            urllib.request.urlretrieve(url, path)
+
+
+    def extract_master_tarball(self):
+        """Unpack tarball of tarballs
+        """
+        with tarfile.open(os.path.join(ISOCHRONES, self.master_tarball_file)) as tar:
+            logging.info('Extracting {}...'.format(self.master_tarball_file))
+            tar.extractall(ISOCHRONES)
+
+    # Specific
+    def phot_tarball_file(self, phot, **kwargs):
+        return os.path.join(self.datadir, '{}.tgz'.format(phot))
+
+    def extract_phot_tarball(self, phot, **kwargs):
+        phot_tarball = self.phot_tarball_file(phot)
+        with tarfile.open(phot_tarball) as tar:
+            logging.info('Extracting {}.tgz...'.format(phot))
+            tar.extractall(self.datadir)
+
+    # Specific
+    def get_filenames(self, phot, afe='afep0', y=''):
+        if not os.path.exists(os.path.join(self.datadir, 'isochrones', phot)):
+            if not os.path.exists(self.phot_tarball_file(phot, afe=afe, y=y)):
+                extract_master_tarball()
+            extract_phot_tarball(phot)
+
+        return glob.glob('{3}/isochrones/{0}/*{1}{2}.{0}*'.format(phot,afe,y,self.datadir))
+
+    # Specific
+    def get_feh(self, filename):
+        m = re.search('feh([mp])(\d+)afe', filename)
+        if m:
+            sign = 1 if m.group(1)=='p' else -1
+            return float(m.group(2))/10 * sign
+        
+    # Specific
+    def to_df(self, filename):
+        try:
+            rec = np.recfromtxt(filename,skip_header=8,names=True)
+        except:
+            print('Error reading {}!'.format(filename))
+            raise RuntimeError
+        df = pd.DataFrame(rec)
+        
+        n = len(df)
+        ages = np.zeros(n)
+        curage = 0
+        i=0
+        for line in open(filename):
+            m = re.match('#',line)
+            if m:
+                m = re.match('#AGE=\s*(\d+\.\d+)\s+',line)
+                if m:
+                    curage=float(m.group(1))
+            else:
+                if re.search('\d',line):
+                    ages[i]=curage
+                    i+=1
+
+        df['age'] = ages
+        df['feh'] = self.get_feh(filename)
+        return df
+
+    # Specific (in sorting)
+    def df_all(self, phot, afe='afep0', y=''):
+        df = pd.concat([self.to_df(f) for f in self.get_filenames(phot, afe=afe, y=y)])
+        return df.sort_values(by=['age','feh','MMo','EEP'])
+        
+    def hdf_filename(self, phot, afe='afep0', y=''):
+        afe_str = '_{}'.format(afe) if afe!='afep0' else ''
+        return os.path.join(self.datadir,'{}{}.h5'.format(phot, afe_str, y))
+
+    def get_hdf(self, phot, afe='afep0', y=''):
+        h5file = self.hdf_filename(phot, afe=afe, y=y)
+        try:
+            df = pd.read_hdf(h5file, 'df')
+        except:
+            df = self.write_hdf(phot, afe=afe, y=y)
+        return df
+
+    def write_hdf(self, phot, afe='afep0', y=''):
+        df = self.df_all(phot, afe=afe, y=y)   
+        h5file = self.hdf_filename(phot, afe=afe, y=y)
+        df.to_hdf(h5file,'df')
+        print('{} written.'.format(h5file))
         return df
 
