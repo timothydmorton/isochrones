tri file corrected; changed to distance^-2 weighting

isochrones/dartmouth/tri.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 import pandas as pd
 import numpy as np
 import cPickle as pickle
 from scipy.interpolate import LinearNDInterpolator as interpnd
 
 from ..config import ISOCHRONES
-DATADIR = os.path.join(ISOCHRONES, 'dartmouth')
 
 from .grid import DartmouthModelGrid
 
-def write_tri(filename=os.path.join(DATADIR,'dartmouth.tri')):
-    df = DartmouthModelGrid(['g'])
+def write_tri(filename=os.path.join(ISOCHRONES,'dartmouth.tri')):
+    df = DartmouthModelGrid(['g']).df
     N = len(df)
     pts = np.zeros((N,3))
     pts[:,0] = np.array(df['MMo'])
-    pts[:,1] = df['age']
+    pts[:,1] = np.array(df['age'])
     pts[:,2] = np.array(df['feh'])
-    gmags = np.array(df['sdss_g'])
+    gmags = np.array(df['g'])
 
     gfn = interpnd(pts,gmags)
 

isochrones/interp.py
@@ -15,8 +15,11 @@ def interp_box(x, y, z, box, values):
     val = 0
     norm = 0
     for i in range(8):
-        # weight = 1./distance
-        w = 1./sqrt((x-box[i,0])**2 + (y-box[i,1])**2 + (z-box[i, 2])**2)
+        # Inv-sq distance weighting
+        # w = 1./sqrt((x-box[i,0])**2 + (y-box[i,1])**2 + (z-box[i, 2])**2)
+        w = 1./((x-box[i,0])*(x-box[i,0]) + 
+                (y-box[i,1])*(y-box[i,1]) + 
+                (z-box[i, 2])*(z-box[i, 2]))
         val += w * values[i]
         norm += w
     
@@ -85,76 +88,81 @@ def interp_values(mass_arr, age_arr, feh_arr, icol,
     Nfeh = len(fehs)
 
     for i in range(N):
-        mass = mass_arr[i]
-        age = age_arr[i]
-        feh = feh_arr[i]
+        results[i] = interp_value(mass_arr[i], age_arr[i], feh_arr[i], icol, 
+                                 grid, mass_col, ages, fehs, grid_Ns, False)
 
-        ifeh = searchsorted(fehs, Nfeh, feh)
-        iage = searchsorted(ages, Nage, age)
-        if ifeh==0 or iage==0 or ifeh==Nfeh or iage==Nage:
-            results[i] = np.nan
-            continue
+        ## Things are slightly faster if the below is used, but for consistency,
+        ## using above.
+        # mass = mass_arr[i]
+        # age = age_arr[i]
+        # feh = feh_arr[i]
 
-        pts = np.zeros((8,3))
-        vals = np.zeros(8)
+        # ifeh = searchsorted(fehs, Nfeh, feh)
+        # iage = searchsorted(ages, Nage, age)
+        # if ifeh==0 or iage==0 or ifeh==Nfeh or iage==Nage:
+        #     results[i] = np.nan
+        #     continue
 
-        i_f = ifeh - 1
-        i_a = iage - 1
-        Nmass = grid_Ns[i_f, i_a]
-        imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
-        pts[0, 0] = grid[i_f, i_a, imass, mass_col]
-        pts[0, 1] = ages[i_a]
-        pts[0, 2] = fehs[i_f]
-        vals[0] = grid[i_f, i_a, imass, icol]
-        pts[1, 0] = grid[i_f, i_a, imass-1, mass_col]
-        pts[1, 1] = ages[i_a]
-        pts[1, 2] = fehs[i_f]
-        vals[1] = grid[i_f, i_a, imass-1, icol]
+        # pts = np.zeros((8,3))
+        # vals = np.zeros(8)
 
-        i_f = ifeh - 1
-        i_a = iage 
-        Nmass = grid_Ns[i_f, i_a]
-        imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
-        pts[2, 0] = grid[i_f, i_a, imass, mass_col]
-        pts[2, 1] = ages[i_a]
-        pts[2, 2] = fehs[i_f]
-        vals[2] = grid[i_f, i_a, imass, icol]
-        pts[3, 0] = grid[i_f, i_a, imass-1, mass_col]
-        pts[3, 1] = ages[i_a]
-        pts[3, 2] = fehs[i_f]
-        vals[3] = grid[i_f, i_a, imass-1, icol]
+        # i_f = ifeh - 1
+        # i_a = iage - 1
+        # Nmass = grid_Ns[i_f, i_a]
+        # imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
+        # pts[0, 0] = grid[i_f, i_a, imass, mass_col]
+        # pts[0, 1] = ages[i_a]
+        # pts[0, 2] = fehs[i_f]
+        # vals[0] = grid[i_f, i_a, imass, icol]
+        # pts[1, 0] = grid[i_f, i_a, imass-1, mass_col]
+        # pts[1, 1] = ages[i_a]
+        # pts[1, 2] = fehs[i_f]
+        # vals[1] = grid[i_f, i_a, imass-1, icol]
 
-        i_f = ifeh
-        i_a = iage - 1
-        Nmass = grid_Ns[i_f, i_a]
-        imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
-        pts[4, 0] = grid[i_f, i_a, imass, mass_col]
-        pts[4, 1] = ages[i_a]
-        pts[4, 2] = fehs[i_f]
-        vals[4] = grid[i_f, i_a, imass, icol]
-        pts[5, 0] = grid[i_f, i_a, imass-1, mass_col]
-        pts[5, 1] = ages[i_a]
-        pts[5, 2] = fehs[i_f]
-        vals[5] = grid[i_f, i_a, imass-1, icol]
+        # i_f = ifeh - 1
+        # i_a = iage 
+        # Nmass = grid_Ns[i_f, i_a]
+        # imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
+        # pts[2, 0] = grid[i_f, i_a, imass, mass_col]
+        # pts[2, 1] = ages[i_a]
+        # pts[2, 2] = fehs[i_f]
+        # vals[2] = grid[i_f, i_a, imass, icol]
+        # pts[3, 0] = grid[i_f, i_a, imass-1, mass_col]
+        # pts[3, 1] = ages[i_a]
+        # pts[3, 2] = fehs[i_f]
+        # vals[3] = grid[i_f, i_a, imass-1, icol]
 
-        i_f = ifeh 
-        i_a = iage
-        Nmass = grid_Ns[i_f, i_a]
-        imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
-        pts[6, 0] = grid[i_f, i_a, imass, mass_col]
-        pts[6, 1] = ages[i_a]
-        pts[6, 2] = fehs[i_f]
-        vals[6] = grid[i_f, i_a, imass, icol]
-        pts[7, 0] = grid[i_f, i_a, imass-1, mass_col]
-        pts[7, 1] = ages[i_a]
-        pts[7, 2] = fehs[i_f]
-        vals[7] = grid[i_f, i_a, imass-1, icol]
+        # i_f = ifeh
+        # i_a = iage - 1
+        # Nmass = grid_Ns[i_f, i_a]
+        # imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
+        # pts[4, 0] = grid[i_f, i_a, imass, mass_col]
+        # pts[4, 1] = ages[i_a]
+        # pts[4, 2] = fehs[i_f]
+        # vals[4] = grid[i_f, i_a, imass, icol]
+        # pts[5, 0] = grid[i_f, i_a, imass-1, mass_col]
+        # pts[5, 1] = ages[i_a]
+        # pts[5, 2] = fehs[i_f]
+        # vals[5] = grid[i_f, i_a, imass-1, icol]
+
+        # i_f = ifeh 
+        # i_a = iage
+        # Nmass = grid_Ns[i_f, i_a]
+        # imass = searchsorted(grid[i_f, i_a, :, mass_col], Nmass, mass)
+        # pts[6, 0] = grid[i_f, i_a, imass, mass_col]
+        # pts[6, 1] = ages[i_a]
+        # pts[6, 2] = fehs[i_f]
+        # vals[6] = grid[i_f, i_a, imass, icol]
+        # pts[7, 0] = grid[i_f, i_a, imass-1, mass_col]
+        # pts[7, 1] = ages[i_a]
+        # pts[7, 2] = fehs[i_f]
+        # vals[7] = grid[i_f, i_a, imass-1, icol]
         
-        results[i] = interp_box(mass, age, feh, pts, vals)
+        # results[i] = interp_box(mass, age, feh, pts, vals)
         
     return results
 
-#@jit(nopython=True)
+@jit(nopython=True)
 def interp_value(mass, age, feh, icol, 
                  grid, mass_col, ages, fehs, grid_Ns, debug):
                  # return_box):
@@ -233,8 +241,14 @@ def interp_value(mass, age, feh, icol,
     pts[7, 2] = fehs[i_f]
     vals[7] = grid[i_f, i_a, imass-1, icol]
     
-    if debug:
-         return pts, vals
-    else:
-        return interp_box(mass, age, feh, pts, vals)
+    # if debug:
+    #     result = np.zeros((8,4))
+    #     for i in range(8):
+    #         result[i, 0] = pts[i, 0]
+    #         result[i, 1] = pts[i, 1]
+    #         result[i, 2] = pts[i, 2]
+    #         result[i, 3] = vals[i]
+    #     return result
+    # else:
+    return interp_box(mass, age, feh, pts, vals)
 