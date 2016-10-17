e20693c585fb04e1df0758e719b3c4e5a3183409
Timothy Morton <tim.morton@gmail.com>
2016-10-17 07:31:40 -0400

    importing ModelGrid in top level

--- isochrones/__init__.py
+++ isochrones/__init__.py
@@ -6,6 +6,7 @@ except NameError:
     __ISOCHRONES_SETUP__ = False
 
 if not __ISOCHRONES_SETUP__:
-    __all__ = ['get_ichrone', 'Isochrone', 'StarModel']
+    __all__ = ['get_ichrone', 'Isochrone', 'StarModel', 'ModelGrid']
     from .isochrone import Isochrone, get_ichrone
     from .starmodel import StarModel 
+    from .grid import ModelGrid
