0c1cf097cdf5e42b8d19c4dec07988b0152f203b
Timothy Morton <tdm@astro.princeton.edu>
2017-10-06 00:00:35 -0400

    add specific values for unit tests

--- isochrones/tests/tests.py
+++ isochrones/tests/tests.py
@@ -14,10 +14,20 @@ MIST = MIST_Isochrone()
 def test_dartmouth_basic(bands=['z', 'B', 'W3', 'LSST_r', 'J', 'UK_J']):
     dar = Dartmouth_Isochrone(bands)
     _basic_ic_checks(dar)
+    print('{} ({})'.format(dar.radius(1., 9.5, 0.0), 0.95409593462955189))
+    assert np.isclose(dar.radius(1., 9.5, 0.0), 0.95409593462955189) 
+    assert np.isclose(dar.radius(1.01, 9.72, 0.02), 1.0435559519926865)
+    assert np.isclose(dar.radius(1.21, 9.38, 0.11), 1.2762022494652547)
+    assert np.isclose(dar.radius(0.61, 9.89, -0.22), 0.5964945760402941)
 
 def test_mist_basic(bands=['G','B','V','J','H','K','W1','W2','W3']):
     ic = MIST_Isochrone(bands)
     _basic_ic_checks(ic)
+    print('{} ({})'.format(ic.radius(1.0, 9.5, 0.0), 0.9764494078461442))
+    assert np.isclose(ic.radius(1.0, 9.5, 0.0), 0.9764494078461442)
+    assert np.isclose(ic.radius(1.01, 9.72, 0.02), 1.0671791635014685)
+    assert np.isclose(ic.radius(1.21, 9.38, 0.11), 1.2836469028034225)
+    assert np.isclose(ic.radius(0.61, 9.89, -0.22), 0.59475269177846402)
 
 def test_spec():
     _check_spec(DAR)
@@ -56,3 +66,6 @@ def _check_spec(ic):
     mod = StarModel(ic, Teff=(5700,100), logg=(4.5, 0.1), feh=(0.0, 0.2))
     assert np.isfinite(mod.lnlike([1.0, 9.6, 0.1, 200, 0.2]))
 
+if __name__=='__main__':
+    test_dartmouth_basic()
+    test_mist_basic()
