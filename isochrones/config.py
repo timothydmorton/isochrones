06fe396533461b1f5751efab85c8cc2dc38a0bfc
Timothy Morton <tdm@astro.princeton.edu>
2018-06-01 15:45:33 -0700

    remove commented code

--- isochrones/config.py
+++ isochrones/config.py
@@ -4,8 +4,5 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 ISOCHRONES = os.getenv('ISOCHRONES',
                        os.path.expanduser(os.path.join('~','.isochrones')))
 
-# if not os.path.exists(ISOCHRONES):
-#     os.makedirs(ISOCHRONES)
-
 POLYCHORD = os.getenv('POLYCHORD',
                     os.path.expanduser(os.path.join('~','PolyChord')))
