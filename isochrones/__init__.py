0c824f01c909ea867eea0843c6c69a3e9f3e8ddd
Timothy Morton <tmorton@ufl.edu>
2019-02-25 11:32:12 -0500

    version bump

--- isochrones/__init__.py
+++ isochrones/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.2dev'
+__version__ = '2.0dev'
 
 try:
     __ISOCHRONES_SETUP__
@@ -7,4 +7,4 @@ except NameError:
 
 if not __ISOCHRONES_SETUP__:
     from .isochrone import get_ichrone
-    from .starmodel import StarModel
+    from .starmodel import StarModel, BinaryStarModel, TripleStarModel
