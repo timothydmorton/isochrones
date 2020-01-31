d8e84ae28bafb3166104b2a94646494016872241
Tim Morton <tim.morton@gmail.com>
2020-01-31 11:13:48 -0800

    change to pytests and use codecov

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -16,8 +16,9 @@ conda create --yes -n test python=$PYTHON_VERSION
 conda activate test
 
 # pip install pandoc=2.0.0
-conda install -c conda-forge numpy numba cython nose pytables pandas
+conda install -c conda-forge numpy numba cython pytables pandas
 conda install -c conda-forge multinest pymultinest
+conda install -c conda-forge pytest codecov
 # conda install -c pyviz pyviz
 # conda install -c conda-forge nbsphinx
 # pip install sphinx_rtd_theme
