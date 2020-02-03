2aaac5aee59ab035795dd81bb2eef5a1596c38b4
Tim Morton <tim.morton@gmail.com>
2020-02-03 13:59:09 -0800

    free memory in initialize script

--- scripts/mist-initialize.py
+++ scripts/mist-initialize.py
@@ -3,5 +3,7 @@ from isochrones import get_ichrone
 mist = get_ichrone("mist", bands="JHK")
 mist.initialize()
 
+del mist
+
 tracks = get_ichrone("mist", bands="JHK", tracks=True)
 tracks.initialize()
